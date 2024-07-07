d4754633b4e1385a74c3a0cdf8cd75678ed48b6d
Nutt Chokwittaya <chokwittaya.nutt@gmail.com>
2024-07-06 22:23:21 -0500

    [4] - add build:dev for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -17,4 +17,11 @@ module.exports = {
     ],
   },
   plugins: [new BundleAnalyzerPlugin()],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
 };
